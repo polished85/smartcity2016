06f162a368e1e9b2dc95604b740c17bccf5a235b
Byron Ponce <byron@polished-web.com>
2016-05-21 10:57:32 -0700

    add dashboard

---
+++ dist/js/dashboard.js
@@ -0,0 +1,14 @@
+
+var html = `
+	<div class="container">
+		<h2>Dashboard</h2>
+		<div class="row">
+			<div class="col-xs-12">
+				<a href="" class="btn btn-block btn-default">Create Event</a>
+				<a href="" class="btn btn-block btn-default">Find Event</a>
+				<a href="" class="btn btn-block btn-default">My Events</a>
+			</div>
+		</div>
+	</div>
+`
+document.getElementById('dashboard').innerHTML = html
