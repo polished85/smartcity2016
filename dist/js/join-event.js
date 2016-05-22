c0241eac1ceb3ccc9c88a868b76a9ef427e7eb35
Byron Ponce <byron@polished-web.com>
2016-05-22 12:29:55 -0700

    update link destination

--- dist/js/join-event.js
+++ dist/js/join-event.js
@@ -2,6 +2,7 @@
 var eventId = window.location.hash.substring(1)
 console.log('eventId: ', eventId)
 console.log('userId', localStorage.getItem('userid'))
+var attendees
 events
 .find(eventId)
 .fetch()
@@ -19,22 +20,51 @@ events
 				<div class="col-xs-12 col-sm-6">
 					<button id="join_as_passanger" class="btn btn-block btn-primary">Passanger</button>
 				</div>
+			</div>
 		`
+		attendees = event.attendees || []
 		var eventEl = document.getElementById('join_event')
 		var eventContent = document.createElement('div')
 		eventContent.className = 'container'
 		eventContent.innerHTML = markup
 		eventEl.appendChild(eventContent)
+
+		document.getElementById('join_as_driver').onclick = function(e){
+			console.log('joining as driver&hellip;')
+			var userId = localStorage.getItem('userid')
+			var attendee = {
+				user: userId,
+				type: 'driver',
+				users: []
+			}
+			attendees.push(attendee)
+			events
+			.replace({
+		    id: event.id,
+		    name: event.name,
+		    location: event.location,
+		    date: event.date,
+		    time: event.time,
+		    private: event.private,
+		    attendees: attendees
+			})
+			.subscribe(
+				(event) => {
+					console.log(event)
+					window.location.pathname = 'event-driver.html'
+					window.location.hash = event.id
+				},
+				(err) => {
+					console.log(err)
+				}
+			)
+		}
+
+		document.getElementById('join_as_passanger').onclick = function(){
+			
+		}
 	},
 	(err) => {
 		console.log(err)
 	}
 )
-
-document.getElementById('join_as_driver').onclick = function(){
-
-}
-
-document.getElementById('join_as_passanger').onclick = function(){
-	
-}
